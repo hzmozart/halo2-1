6278b2346564402f589c23dbea47d77cf9dbb65a
Zhang Junyu <junyu92@gmail.com>
2022-03-21 06:43:16 +0000

    export

--- halo2_proofs/src/plonk/circuit.rs
+++ halo2_proofs/src/plonk/circuit.rs
@@ -1048,7 +1048,7 @@ impl<F: Field> ConstraintSystem<F> {
         panic!("get_instance_query_index called for non-existent query");
     }
 
-    pub(crate) fn get_any_query_index(&self, column: Column<Any>, at: Rotation) -> usize {
+    pub fn get_any_query_index(&self, column: Column<Any>, at: Rotation) -> usize {
         match column.column_type() {
             Any::Advice => {
                 self.get_advice_query_index(Column::<Advice>::try_from(column).unwrap(), at)
